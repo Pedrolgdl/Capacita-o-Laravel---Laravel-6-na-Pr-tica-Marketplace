5c09fcb1dfc0c1e9d96ab46a0e0ee3d8de929e96
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2021-12-04 16:01:39 -0300

    Terminando o mapeamento N:N entre a tabela categories e a tabela products

--- app/Product.php
+++ app/Product.php
@@ -3,11 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\FuncCall;
 
 class Product extends Model
 {
     public function store()
     {
         return $this->belongsTo(Store::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
     }
 }
