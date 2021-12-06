c2756d1b06d51aa99a5ca5b70911760d66532930
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2021-12-06 10:33:50 -0300

    Atualizando a Seed dos Users para que ela faça, além de criar 40 usuários, criar uma Store para cada um chamando por meio do vinculo entre User e Store (store()) a factory de Store. Também criando Seed para Store, que pegas as lojas feitas junto com os usuários e atribui um produto para cada

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -10,7 +10,7 @@ $factory->define(\App\Product::class, function (Faker $faker) {
         'name' => $faker->name,
         'description' => $faker->sentence,
         'body' => $faker->paragraph(5, true),
-        'price' => $faker->randomFloat(2,10),
+        'price' => $faker->randomFloat(2, 1, 10),
         'slug' => $faker->slug,
     ];
 });
