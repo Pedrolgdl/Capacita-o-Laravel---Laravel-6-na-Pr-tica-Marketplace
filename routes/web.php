7a0024d3cedf3133d7fe2c4c9c17ea78e94b943d
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2021-12-08 14:32:42 -0300

    Simplificando rotas

--- routes/web.php
+++ routes/web.php
@@ -104,6 +104,14 @@ Route::get('/model', function () {
     return $product->categories;
 });
 
-Route::get('/admin/stores', 'Admin\\StoreController@index');
-Route::get('/admin/stores/create', 'Admin\\StoreController@create');
-Route::post('/admin/stores/store', 'Admin\\StoreController@store');
+Route::prefix('admin')->namespace('Admin')->group(function(){
+
+    Route::prefix('stores')->group(function(){
+
+        Route::get('/', 'StoreController@index');
+        Route::get('/create', 'StoreController@create');
+        Route::post('/store', 'StoreController@store');
+        
+    });
+
+});
