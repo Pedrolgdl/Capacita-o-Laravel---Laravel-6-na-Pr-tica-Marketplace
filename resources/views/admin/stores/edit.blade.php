81d3d34b6e3310fd611d8603d79e0fc99fc16c4e
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2021-12-08 22:34:25 -0300

    Criando rotas, views e métodos para atualizar dados de uma Store ou deletar

---
+++ resources/views/admin/stores/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Criar Loja</h1>
+    <form action="/admin/stores/update/{{$store->id}}" method="post">
+        <input type="hidden" name="_token" value="{{csrf_token()}}">
+        <div class="form-group">
+            <label>Nome Loja</label>
+            <input type="text" name="name" class="form-control" value="{{$store->name}}">
+        </div>
+
+        <div class="form-group">
+            <label>Descrição</label>
+            <input type="text" name="description" class="form-control" value="{{$store->description}}">
+        </div>
+
+        <div class="form-group">
+            <label>Telefone</label>
+            <input type="text" name="phone" class="form-control" value="{{$store->phone}}">
+        </div>
+        
+        <div class="form-group">
+            <label>Celular/WhatsApp</label>
+            <input type="text" name="mobile_phone" class="form-control" value="{{$store->mobile_phone}}">
+        </div>
+
+        <div class="form-group">
+            <label>slug</label>
+            <input type="text" name="slug" class="form-control" value="{{$store->slug}}">
+        </div>        
+
+        <div>
+            <button type="submit" class="btn btn-lg btn-success">Atualizar Loja</button>
+        </div>
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -111,7 +111,10 @@ Route::prefix('admin')->namespace('Admin')->group(function(){
         Route::get('/', 'StoreController@index');
         Route::get('/create', 'StoreController@create');
         Route::post('/store', 'StoreController@store');
-        
+        Route::get('/{store}/edit', 'StoreController@edit');
+        Route::post('/update/{store}', 'StoreController@update');
+        Route::get('/destroy/{store}', 'StoreController@destroy');
+
     });
 
 });
