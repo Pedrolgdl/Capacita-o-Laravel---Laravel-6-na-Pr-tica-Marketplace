48e2d7082b2f8b7c8158fdf8d13b64a3995d6bca
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2021-12-09 13:55:57 -0300

    Implementando método de destroy para deletar produtos. Colocamos o botão de deletar dentro de um formulário, par usarmos o @method como DELETE

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -106,6 +106,10 @@ class ProductController extends Controller
      */
     public function destroy($product)
     {
-        return $product;
+        $product = $this->product->find($product);
+        $product->delete();
+
+        flash('Produto Removido com Sucesso!')->success();
+        return redirect()->route('admin.products.index');
     }
 }
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -17,9 +17,16 @@
                 <tr>
                     <td>{{$p->id}}</td>
                     <td>{{$p->name}}</td>
+                    <td>R$ {{number_format($p->price, 2, ',', '.')}}</td>
                     <td>
-                        <a href="{{route('admin.products.edit', ['product' => $p->id])}}" class="btn btn-sm btn-primary">EDITAR</a>
-                        <a href="{{route('admin.products.destroy', ['product' => $p->id])}}" class="btn btn-sm btn-danger">REMOVER</a>
+                        <div class="btn-group">
+                            <a href="{{route('admin.products.edit', ['product' => $p->id])}}" class="btn btn-sm btn-primary">EDITAR</a>
+                            <form action="{{route('admin.products.destroy', ['product' => $p->id])}}" method="post">
+                                @csrf
+                                @method("DELETE")
+                                <button type="submit" class="btn btn-sm btn-danger">REMOVER</button>
+                            </form>
+                        </div>
                     </td>
                 </tr>
             @endforeach
