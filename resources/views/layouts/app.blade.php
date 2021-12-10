7d237e7093e8f9bd21d642459515fee970bf0b18
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2021-12-10 14:56:44 -0300

    Implementando diretiva Auth para que alguns itens da navbar só apareçam se o usuário estiver logado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,27 +14,29 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item @if(request()->is('admin/stores')) active @endif">
-                    <a class="nav-link" href="{{route('admin.stores.index')}}">Lojas <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item @if(request()->is('admin/products')) active @endif">
-                    <a class="nav-link" href="{{route('admin.products.index')}}">Produtos</a>
-                </li>
-            </ul>
-
-            <div class="my-2 my-lg-0">
+        @auth
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" onclick="event.preventDefault(); document.querySelector('form.logout').submit(); ">Sair</a>
-
-                        <form action="{{route('logout')}}" class="logout" method="POST" style="display: none;">
-                            @csrf
-                        </form>
+                    <li class="nav-item @if(request()->is('admin/stores')) active @endif">
+                        <a class="nav-link" href="{{route('admin.stores.index')}}">Lojas <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item @if(request()->is('admin/products')) active @endif">
+                        <a class="nav-link" href="{{route('admin.products.index')}}">Produtos</a>
                     </li>
                 </ul>
-            </div>
+
+                <div class="my-2 my-lg-0">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" onclick="event.preventDefault(); document.querySelector('form.logout').submit(); ">Sair</a>
+
+                            <form action="{{route('logout')}}" class="logout" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            @endauth
         </div>
     </nav>
     
