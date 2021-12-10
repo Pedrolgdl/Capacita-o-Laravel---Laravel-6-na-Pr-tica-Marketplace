8692d04dc1b026002cbe03c34d445e57db8c24a1
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2021-12-10 15:09:22 -0300

    Arrumando as rotas de redirecionamento dos controladores de login, registro e reset de senha

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/admin/products';
 
     /**
      * Create a new controller instance.
