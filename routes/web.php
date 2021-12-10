15666e10764a8af08cb2586634ccc96122a1fc33
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2021-12-10 11:13:33 -0300

    Adicionando controle de acesso ao admin (admin/stores e admin/products) pela middleware auth, que faz a autenticação do usuário

--- routes/web.php
+++ routes/web.php
@@ -104,24 +104,26 @@ Route::get('/model', function () {
     return $product->categories;
 });
 
-Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
+Route::group(['middleware' => ['auth']], function() {
 
-    // Route::prefix('stores')->name('stores.')->group(function(){
+    Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function() {
 
-    //     Route::get('/', 'StoreController@index')->name('index');
-    //     Route::get('/create', 'StoreController@create')->name('create');
-    //     Route::post('/store', 'StoreController@store')->name('store');
-    //     Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
-    //     Route::post('/update/{store}', 'StoreController@update')->name('update');
-    //     Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
-
-    // });
-
-    Route::resource('stores', 'StoreController');
-    Route::resource('products', 'ProductController');
+        // Route::prefix('stores')->name('stores.')->group(function(){
+        //     Route::get('/', 'StoreController@index')->name('index');
+        //     Route::get('/create', 'StoreController@create')->name('create');
+        //     Route::post('/store', 'StoreController@store')->name('store');
+        //     Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
+        //     Route::post('/update/{store}', 'StoreController@update')->name('update');
+        //     Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
+        // });
+    
+        Route::resource('stores', 'StoreController');
+        Route::resource('products', 'ProductController');
+    
+    });
 
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');//->middleware('auth');
