2e36812978f4eab8be030b2296bc98d54cf4a455
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2021-12-13 11:00:53 -0300

    Fazendo os mesmo passos feitos para Store agora para Products. Corrigindo erro de menu na navbar não estar ativo em páginas dentro de produtos e lojas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,10 @@
         @auth
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item @if(request()->is('admin/stores')) active @endif">
+                    <li class="nav-item @if(request()->is('admin/stores*')) active @endif">
                         <a class="nav-link" href="{{route('admin.stores.index')}}">Lojas <span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item @if(request()->is('admin/products')) active @endif">
+                    <li class="nav-item @if(request()->is('admin/products*')) active @endif">
                         <a class="nav-link" href="{{route('admin.products.index')}}">Produtos</a>
                     </li>
                 </ul>
