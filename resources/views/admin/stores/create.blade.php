bcbfd28aa2b62362514a8f1a39d83b9f65d7be68
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2021-12-13 10:04:39 -0300

    Adicionando função old, que retorna o texto digitado pelo usuário, sem perder informações nos campos dos formulários

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -6,7 +6,7 @@
         <input type="hidden" name="_token" value="{{csrf_token()}}">
         <div class="form-group">
             <label>Nome Loja</label>
-            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
 
             @error('name')
                 <div class="invalid-feedback">
@@ -17,7 +17,7 @@
 
         <div class="form-group">
             <label>Descrição</label>
-            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror">
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" value="{{old('description')}}">
 
             @error('description')
                 <div class="invalid-feedback">
@@ -28,7 +28,7 @@
 
         <div class="form-group">
             <label>Telefone</label>
-            <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror">
+            <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror" value="{{old('phone')}}">
 
             @error('phone')
                 <div class="invalid-feedback">
@@ -39,7 +39,7 @@
         
         <div class="form-group">
             <label>Celular/WhatsApp</label>
-            <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror">
+            <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror" value="{{old('mobile_phone')}}">
 
             @error('mobile_phone')
                 <div class="invalid-feedback">
