c60c8f2e287165b93fcd24e5f2f74ed8e80f7310
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2021-12-13 09:55:24 -0300

    Adicionando mensagens de erro nos campos que tem validações

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -6,22 +6,46 @@
         <input type="hidden" name="_token" value="{{csrf_token()}}">
         <div class="form-group">
             <label>Nome Loja</label>
-            <input type="text" name="name" class="form-control">
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+
+            @error('name')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label>Descrição</label>
-            <input type="text" name="description" class="form-control">
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror">
+
+            @error('description')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label>Telefone</label>
-            <input type="text" name="phone" class="form-control">
+            <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror">
+
+            @error('phone')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
         
         <div class="form-group">
             <label>Celular/WhatsApp</label>
-            <input type="text" name="mobile_phone" class="form-control">
+            <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror">
+
+            @error('mobile_phone')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
 
         <div class="form-group">
