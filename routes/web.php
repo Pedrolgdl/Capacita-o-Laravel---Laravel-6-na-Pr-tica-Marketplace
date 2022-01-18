3407584483b2e84195c38a9394a1c5a917c290e0
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2022-01-18 10:29:58 -0300

    Criando CRUD para categorias

--- routes/web.php
+++ routes/web.php
@@ -119,6 +119,7 @@ Route::group(['middleware' => ['auth']], function() {
     
         Route::resource('stores', 'StoreController');
         Route::resource('products', 'ProductController');
+        Route::resource('categories', 'CategoryController');
     
     });
 
